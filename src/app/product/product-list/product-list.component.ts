b38830c946366b0c5ceb753f7a5a7acf44cf4468
ChinamiIchikawa <fujinetichikawa@gmail.com>
2021-01-27 18:25:01 +0900

    フロントとバックの結合

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,12 +7,15 @@ import { products } from '../../products';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-   products: any = [1,2,3,4]
+   products: any
 
-  constructor() { }
+  constructor(private ProductService: ProductService) { }
 
   ngOnInit(): void {
-    this.products = products
+    const productsObservable = this.ProductService.getProducts()
+    productsObservable.subscribe(
+      (data) => {this.products = data},
+      (err) => {console.error('次のエラーが発生しました: ' + err)}
+    )
   }
-
 }
